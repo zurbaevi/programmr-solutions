78fc83151b6384c4c997bf64cab905a8f28aadef
Nika <zurbaevi.nika@gmail.com>
2020-11-07 22:47:17 +0400

    Added solutions

---
+++ src/main/java/com/programmr/java/miscellaneous/Operators.java
@@ -0,0 +1,15 @@
+package com.programmr.java.miscellaneous;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class Operators {
+    public static void main(String[] args) {
+        int number = 15;
+        int number2 = 9;
+        int calculatedResult = 0;
+        System.out.println("Muliplication of the 2 numbers is:");
+        calculatedResult = number * number2;
+        System.out.println(calculatedResult);
+    }
+}
---
+++ src/main/java/com/programmr/java/miscellaneous/WordReverse.java
@@ -0,0 +1,20 @@
+package com.programmr.java.miscellaneous;
+
+import java.util.Scanner;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class WordReverse {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string:\n");
+        String s;
+        s = input.nextLine();
+        String rev = "";
+        StringBuilder stringBuilder = new StringBuilder(s);
+        stringBuilder.reverse();
+        rev = stringBuilder.toString();
+        System.out.println("Reverse:\n" + rev);
+    }
+}

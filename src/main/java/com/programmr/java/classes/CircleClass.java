2ef071e29323d963a30ab5dea0dbff71991f43c3
Nika <zurbaevi.nika@gmail.com>
2020-11-14 20:34:46 +0400

    Added solutions

---
+++ src/main/java/com/programmr/java/classes/CircleClass.java
@@ -0,0 +1,36 @@
+package com.programmr.java.classes;
+
+import java.util.Scanner;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class CircleClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Enter radius:");
+        double rad = scanner.nextDouble();
+        System.out.println("Enter color:");
+        String col = scanner.next();
+
+        MyCircle mc = new MyCircle(rad, col);
+
+        System.out.println(mc.getArea());
+        System.out.println(mc.color);
+    }
+}
+
+class MyCircle {
+    double rad;
+    String color;
+
+    public MyCircle(double rad, String color) {
+        this.rad = rad;
+        this.color = color;
+    }
+
+    public double getArea() {
+        return Math.PI * (rad * rad);
+    }
+}

f7afa26b5d852e01c5a1c5c69b9102cf45ec7b1a
Nika <zurbaevi.nika@gmail.com>
2020-11-15 23:17:27 +0400

    Added solutions

---
+++ src/main/java/com/programmr/java/variables/VariableMaxValues.java
@@ -0,0 +1,11 @@
+package com.programmr.java.variables;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class VariableMaxValues {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}

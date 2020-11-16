03fbed63b00343466f0664f16fbe8d3349ac831d
Nika <zurbaevi.nika@gmail.com>
2020-11-16 15:09:06 +0400

    Added solutions

---
+++ src/main/java/com/programmr/java/variables/DeclaringVariables.java
@@ -0,0 +1,16 @@
+package com.programmr.java.variables;
+
+/**
+ * @author Nika Zurbaevi
+ */
+public class DeclaringVariables {
+    public static void main(String[] args){
+        int a1 = 63;
+        float b1 = 45.0F;
+        double c1 = 67.675586;
+
+        System.out.println("The value of first variable is "+a1);
+        System.out.println("The value of second variable is "+b1);
+        System.out.println("The value of third variable is "+c1);
+    }
+}
